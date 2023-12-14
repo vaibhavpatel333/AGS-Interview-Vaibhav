a51e6ea306f1f406dd0c2c4871994eb44656c222
vaibhav <vaibhavpatel2017@gmail.com>
2023-12-14 20:34:28 +0530

    two sum

--- src/selection-sort/solution.js
+++ src/selection-sort/solution.js
@@ -4,6 +4,23 @@
  * @returns {number[]} - The sorted array.
  */
 function selectionSort(arr) {
+
+    let n = arr.length;
+    for (let i=0; i<n; i++){
+
+        let init = 1
+        for (let j = 0; j < n; j++) {
+            if (arr[j] < arr[init]){
+                init = j
+            }
+
+        }
+
+        [arr[i], arr[init]] = []
+
+
+    }
+    return arr
     // Candidate's solution goes here
 }
 
--- src/two-sum/solution.js
+++ src/two-sum/solution.js
@@ -5,6 +5,17 @@
  * @returns {number[]} - The indices of the two numbers.
  */
 function twoSum(nums, target) {
+
+    const n = nums.length;
+    for (let i = 0; i < n-1; i++) {
+        for (let j = i + 1; j < n; j++) {
+            
+            if(nums[i] + nums[j] === target){
+                return [i, j]
+            }
+        }
+    }
+    return []
     // Implement your solution here
 }
 
